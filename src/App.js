5a79498d17ea263effaa204eebfad7e193197043
Pranav <kadampranav2003@gmail.com>
2022-11-25 23:18:45 +0530

    updated api calls

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
+import Login from './Components/Login';
+import SignUp from './Components/SignUp'
+import Editor from './Components/Editor';
+import Profile from './Components/Profile';
+import Home from './Components/Home';
 
 function App() {
+
+  let session_data = localStorage.getItem('user')
+  console.log(session_data)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+ <div className="App">
+      
+      <BrowserRouter>
+      
+      <Routes>
+        <Route path='/' element={<Home/>}/>
+        <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<SignUp/>} />
+        
+        <Route path="/editor" element={<Editor/>} />
+        
+        
+        <Route path="/profile" element={<Profile/>}/>
+
+      </Routes>
+      </BrowserRouter>
+      
     </div>
   );
 }
